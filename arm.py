592827703a16e036101c4aa6d68ce0901dcecd61
BlockOG <alesiobaca@gmail.com>
2022-06-27 15:24:29 +0300

    Make the program stop when requested

--- arm.py
+++ arm.py
@@ -1,52 +1,54 @@
 # LEGO type:standard slot:16
-from spike import Motor, ColorSensor, PrimeHub
-from spike.control import wait_for_seconds
+from spike import Motor, ColorSensor
 
 base = Motor("A")
 arm = Motor("B")
 claws = Motor("C")
 sensor = ColorSensor("D")
 
-base.set_default_speed(15)
-arm.set_default_speed(10)
-claws.set_default_speed(20)
+try:
+    base.set_default_speed(15)
+    arm.set_default_speed(10)
+    claws.set_default_speed(20)
 
-base.set_stop_action(Motor.HOLD)
-arm.set_stop_action(Motor.HOLD)
+    base.set_stop_action(Motor.HOLD)
+    arm.set_stop_action(Motor.HOLD)
 
-color_map = {
-    "blue": 0,
-    "green": 1,
-    "violet": 1,
-    "red": 1,
-    "yellow": 2,
-}
+    color_map = {
+        "yellow": 0,
+        "green": 1,
+        "violet": 1,
+        "red": 1,
+        "blue": 2,
+    }
 
-while True:
+    while True:
+        claws.run_to_position(0)
+        arm.run_to_position(45)
+
+        base.run_to_position(0)
+
+        arm.run_to_position(15)
+        claws.run_to_position(135)
+
+        color = sensor.get_color()
+
+        arm.run_to_position(45)
+
+        if color == None:
+            continue
+        elif color in color_map:
+            base.run_to_position(color_map[color] * 90 + 90)
+        else:
+            continue
+
+        arm.run_to_position(15)
+        claws.run_to_position(0)
+        arm.run_to_position(45)
+except KeyboardInterrupt:
     claws.run_to_position(0)
-    arm.run_to_position(45)
-
     base.run_to_position(0)
-
     arm.run_to_position(15)
-    claws.run_to_position(135)
     
-    color = sensor.get_color()
-    
-    arm.run_to_position(45)
-
-    if color == None:
-        break
-    elif color in color_map:
-        base.run_to_position(color_map[color] * 90 + 90)
-    else:
-        break
-
-    arm.run_to_position(15)
-    claws.run_to_position(0)
-    arm.run_to_position(45)
-
-base.set_stop_action(Motor.BRAKE)
-arm.set_stop_action(Motor.BRAKE)
-claws.run_to_position(0)
-arm.run_to_position(15)
+    base.set_stop_action(Motor.BRAKE)
+    arm.set_stop_action(Motor.BRAKE)
