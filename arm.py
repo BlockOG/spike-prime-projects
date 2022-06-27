b993a91790b7cd9edcad7b17bb7884befefd29dc
BlockOG <alesiobaca@gmail.com>
2022-06-27 14:17:08 +0300

    Some stuff

---
+++ arm.py
@@ -0,0 +1,43 @@
+# LEGO type:standard slot:16
+from spike import Motor, ColorSensor, PrimeHub
+from spike.control import wait_for_seconds
+
+base = Motor("A")
+arm = Motor("B")
+claws = Motor("C")
+sensor = ColorSensor("D")
+
+base.set_default_speed(15)
+arm.set_default_speed(10)
+claws.set_default_speed(20)
+
+base.set_stop_action(Motor.HOLD)
+arm.set_stop_action(Motor.HOLD)
+
+while True:
+    claws.run_to_position(0)
+    arm.run_to_position(45)
+
+    base.run_to_position(0)
+
+    arm.run_to_position(15)
+    claws.run_to_position(135)
+    
+    color = sensor.get_color()
+    
+    arm.run_to_position(45)
+
+    if color == "red":
+        base.run_to_position(180)
+    elif color == "green":
+        base.run_to_position(180)
+    elif color == "blue":
+        base.run_to_position(90)
+    elif color == "yellow":
+        base.run_to_position(270)
+    else:
+        break
+
+    arm.run_to_position(15)
+    claws.run_to_position(0)
+    arm.run_to_position(45)
--- snake.py
+++ snake.py
@@ -1,6 +1,7 @@
-# LEGO type:standard slot:6 autostart
+# LEGO type:advanced slot:6 autostart
 from spike.control import wait_for_seconds
 from random import randint
+from runtime.virtualmachine import VirtualMachine
 import hub
 
 sn_pos = (0, 0)
@@ -10,74 +11,80 @@ got_apple = False
 snake = []
 apple = None
 
-hub.button.left.callback(
-    lambda time: (
-        globals().__setitem__("direction", (direction - 1) % 4),
-        globals().__setitem__("changed", True),
-    )
-    if time == 0 and not changed
-    else None
-)
-hub.button.right.callback(
-    lambda time: (
-        globals().__setitem__("direction", (direction + 1) % 4),
-        globals().__setitem__("changed", True),
-    )
-    if time == 0 and not changed
-    else None
-)
 
-while True:
-    changed = False
-    hub.display.clear()
-    hub.display.pixel(sn_pos[0], sn_pos[1], 9)
+async def right_pressed(vm, stack):
+    global direction, changed
+    if changed:
+        direction = (direction + 1) % 4
+        changed = True
 
-    if direction in (0, 2):
-        hub.display.pixel(sn_pos[0], sn_pos[1] - 1, 4)
-        hub.display.pixel(sn_pos[0], sn_pos[1] + 1, 4)
-    elif direction in (1, 3):
-        hub.display.pixel(sn_pos[0] - 1, sn_pos[1], 4)
-        hub.display.pixel(sn_pos[0] + 1, sn_pos[1], 4)
 
-    for x, y in snake:
-        hub.display.pixel(x, y, 9)
+async def on_start(vm, stack):
+    global sn_pos, direction, changed, got_apple, snake, apple
 
-    for i in range(5):
-        hub.display.pixel(4, i, 9)
+    while True:
+        changed = False
+        hub.display.clear()
+        hub.display.pixel(sn_pos[0], sn_pos[1], 9)
 
-    if not apple:
-        if len(snake) < 19:
-            apple = (randint(0, 3), randint(0, 4))
-            while apple in snake or apple == sn_pos:
+        if direction in (0, 2):
+            hub.display.pixel(sn_pos[0], sn_pos[1] - 1, 4)
+            hub.display.pixel(sn_pos[0], sn_pos[1] + 1, 4)
+        elif direction in (1, 3):
+            hub.display.pixel(sn_pos[0] - 1, sn_pos[1], 4)
+            hub.display.pixel(sn_pos[0] + 1, sn_pos[1], 4)
+
+        for x, y in snake:
+            hub.display.pixel(x, y, 9)
+
+        for i in range(5):
+            hub.display.pixel(4, i, 9)
+
+        if not apple:
+            if len(snake) < 19:
                 apple = (randint(0, 3), randint(0, 4))
-        else:
-            hub.led((0, 255, 0))
-            break
+                while apple in snake or apple == sn_pos:
+                    apple = (randint(0, 3), randint(0, 4))
+            else:
+                hub.led((0, 255, 0))
+                break
 
-    hub.display.pixel(apple[0], apple[1], 7)
+        hub.display.pixel(apple[0], apple[1], 7)
 
-    wait_for_seconds(0.5)
+        wait_for_seconds(0.5)
 
-    snake.append(sn_pos)
-    if not got_apple:
-        snake.pop(0)
-    got_apple = False
+        snake.append(sn_pos)
+        if not got_apple:
+            snake.pop(0)
+        got_apple = False
 
-    if direction == 0:
-        sn_pos = (sn_pos[0] + 1, sn_pos[1])
-    elif direction == 1:
-        sn_pos = (sn_pos[0], sn_pos[1] + 1)
-    elif direction == 2:
-        sn_pos = (sn_pos[0] - 1, sn_pos[1])
-    elif direction == 3:
-        sn_pos = (sn_pos[0], sn_pos[1] - 1)
+        if direction == 0:
+            sn_pos = (sn_pos[0] + 1, sn_pos[1])
+        elif direction == 1:
+            sn_pos = (sn_pos[0], sn_pos[1] + 1)
+        elif direction == 2:
+            sn_pos = (sn_pos[0] - 1, sn_pos[1])
+        elif direction == 3:
+            sn_pos = (sn_pos[0], sn_pos[1] - 1)
 
-    if sn_pos in snake:
-        raise SystemExit
+        if sn_pos in snake:
+            raise SystemExit
 
-    if sn_pos == apple:
-        apple = None
-        got_apple = True
+        if sn_pos == apple:
+            apple = None
+            got_apple = True
 
-    if sn_pos[0] < 0 or sn_pos[0] >= 4 or sn_pos[1] < 0 or sn_pos[1] >= 5:
-        raise SystemExit
+        if sn_pos[0] < 0 or sn_pos[0] >= 4 or sn_pos[1] < 0 or sn_pos[1] >= 5:
+            raise SystemExit
+
+
+def setup(rpc, system, stop):
+    vm = VirtualMachine(rpc, system, stop, "snake")
+    vm.register_on_start("snake_on_start", on_start)
+    vm.register_on_button(
+        "snake_on_right_pressed",
+        right_pressed,
+        "right",
+        "pressed",
+    )
+    return vm
